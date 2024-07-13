32b43257547c1cab7c0170a5a779edfbd42ca5ee
maaged <maagedashour@yahoo.com>
2024-07-13 10:49:44 +0300

    Create Home Screen + Scroll View to Keep Content + Finish First Step Top info Tap

--- WGAME/Views/Home.swift
+++ WGAME/Views/Home.swift
@@ -9,8 +9,63 @@ import SwiftUI
 
 struct Home: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ScrollView(.vertical, showsIndicators: false) {
+            VStack(spacing: 8) {
+                //MARK - APP BAR
+                HStack {
+                    //MARK: DRAWER MENU
+                    Button {
+                        
+                    } label: {
+                        Image("wave")
+                            .renderingMode(.original)
+                            .resizable()
+                            .aspectRatio(contentMode: .fit)
+                            .frame(width: 25, height: 25)
+                    } // END DRAWER MENU
+                    
+                    Spacer()
+                    
+                    
+                }
+                .foregroundColor(.black)
+                HStack {
+                    //MARK: DRAWER MENU
+                    Button {
+                        
+                    } label: {
+                        Text("Hello, Mohamed")
+                    } // END DRAWER MENU
+                    
+                    Spacer()
+                    
+                    //MARK: SEARCH ICON
+                    Button {
+                        
+                    } label: {
+                        Image("gamer")
+                            .renderingMode(.original)
+                            .resizable()
+                            .aspectRatio(contentMode: .fit)
+                            .frame(width: 25, height: 25)
+                    } // END AVATAR ICON
+                    
+                    
+                }
+                .foregroundColor(.black)
+                
+                // END APP BAR
+                HStack {
+                    Text("Explore \nGames Giveaways")
+                        .font(.title.bold())
+                    
+                    Spacer()
+                    
+                } // END BODY TOP
+            } .padding()
+        }
     }
+    
 }
 
 struct Home_Previews: PreviewProvider {
